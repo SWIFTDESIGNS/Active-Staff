72b8b371ab47978163b1790bd02dc779f6eadda3
Swift Designs <57051852+SWIFTDESIGNS@users.noreply.github.com>
2020-11-12 12:36:25 +0000

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,40 @@
+RegisterCommand("In", function()
+
+BeginTextCommandThefeedPost("STRING")
+AddTextComponentSubstringPlayerName("You Are Now ~g~Active ~r~Staff")
+EndTextCommandThefeedPostTicker(true, true)
+
+TriggerServerEvent("StaffA")
+
+Citizen.SetTimeout(3000, function()
+    local player = GetPlayerName(playerId)
+    TriggerEvent('chat:addMessage', {
+        color = {255,0,0},
+        multiline = true,
+        args = {'[Server]', player.. ' Is now ~g~Active ~r~Staff'}
+        })
+  end)
+  
+end)
+
+RegisterCommand("Out", function()
+
+  BeginTextCommandThefeedPost("STRING")
+  AddTextComponentSubstringPlayerName("You Are No longer ~r~Active ~r~Staff")
+  EndTextCommandThefeedPostTicker(true, true)
+  
+  TriggerServerEvent("StaffO")
+  
+  Citizen.SetTimeout(3000, function()
+      local player = GetPlayerName(playerId)
+      TriggerEvent('chat:addMessage', {
+          color = {255,0,0},
+          multiline = true,
+          args = {'[Server]', player.. ' Is no longer ~r~Active Staff'}
+          })
+    end)
+    
+  end)
+
+
+
---
+++ server.lua
@@ -0,0 +1,53 @@
+
+PerformHttpRequest(SL, function(err, text, headers) end, 'POST', json.encode({username = "SwiftNotifications", embeds = active}), { ['Content-Type'] = 'application/json' })
+
+print("^7====================")
+print("^1Active Staff System")
+print("^7====================")
+
+
+--Only Edit Below if you know what you are doing.
+
+RegisterServerEvent("StaffA")
+AddEventHandler("StaffA", function(reason)
+    --WebHook Below
+local SL = "https://discord.com/api/webhooks/775792720110420000/LAOo2F5caf3gtkQugMmLn9lR7CgL0_gsE-f5Tv405eCV27_91mqj3c1COJYA3xFjz2O9"
+local Script_name = "Active Staff"
+local day = os.date("%Y/%m/%d")
+local time = os.date("%X")
+local status = "Active"
+local player = GetPlayerName(source)
+local active = {
+    {
+        ["color"] = "16711680",
+        ["title"] = "Staff Logs",
+        ["description"] = "StaffMember: **"..player.."**\nIs Now: **"..status.."**\n Time Clocked in: **"..time.."**\n Day:**"..day.."**",
+        ["footer"] = {
+            ["text"] = Script_name,
+        },
+    }
+}
+PerformHttpRequest(SL, function(err, text, headers) end, 'POST', json.encode({username = "SwiftNotifications", embeds = active}), { ['Content-Type'] = 'application/json' })
+end)
+
+RegisterServerEvent("StaffO")
+AddEventHandler("StaffO", function(reason,SL)
+    --WebHook Below
+local SL = "https://discord.com/api/webhooks/775792720110420000/LAOo2F5caf3gtkQugMmLn9lR7CgL0_gsE-f5Tv405eCV27_91mqj3c1COJYA3xFjz2O9" 
+local Script_name = "Active Staff"
+local day = os.date("%Y/%m/%d")
+local time = os.date("%X")
+local status = "Active"
+local player = GetPlayerName(source)
+local Inactive = {
+    {
+        ["color"] = "16711680",
+        ["title"] = "Staff Logs",
+        ["description"] = "StaffMember: **"..player.."**\nIs No longer: **"..status.."**\n Time Clocked Out: **"..time.."**\n Day:**"..day.."**",
+        ["footer"] = {
+            ["text"] = Script_name,
+        },
+    }
+}
+PerformHttpRequest(SL, function(err, text, headers) end, 'POST', json.encode({username = "SwiftNotifications", embeds = Inactive}), { ['Content-Type'] = 'application/json' })
+end)
