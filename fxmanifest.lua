cec4cd374dc1bcc38a08a83ae70d2314076ebf54
Swift Designs <57051852+SWIFTDESIGNS@users.noreply.github.com>
2021-01-14 08:23:03 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'SwiftDesigns'
+description 'Script with staff abilities'
+
+client_script {
+    'client.lua',
+    'Config.lua'
+}
+server_script {
+    'server.lua',
+    'Config.lua'
+}
+
+    
